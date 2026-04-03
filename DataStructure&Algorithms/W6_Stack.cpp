fa64e70ef6379db8115182c476245f66dae49ddc
s0nble <xxitexteddyxx@gamil.com>
2026-04-03 23:41:37 +0200

    l

--- DataStructure&Algorithms/W6_Stack.cpp
+++ DataStructure&Algorithms/W6_Stack.cpp
@@ -95,17 +95,44 @@ public:
 
         
     }
+
+    //Task 8: peek
+ElementType peek(){
+    if(isEmpty()){
+        cout << "Stack is empty!" <<endl;
+        return -9999999;
+    }
+    return top->element;
+}
 };
 
+
+
 // Task 9: reverse a linked stack using a second stack
-LinkedStack reverse(LinkedStack s1) {
+LinkedStack reverse(LinkedStack &s1) {
+    LinkedStack s2;
+    while(!s1.isEmpty()){
+        s2.push(s1.pop());
+    }
 
-
-    
+return s2;
 }
 
 // Task 10: merge two linked stacks preserving order
-// LinkedStack merge(LinkedStack &s1, LinkedStack &s2) { ... }
+LinkedStack merge(LinkedStack &s1, LinkedStack &s2) {
+    LinkedStack s3;
+    while(!s1.isEmpty()){
+        s3.push(s1.pop());
+    }
+    while(!s2.isEmpty()){
+        s3.push(s2.pop());
+    }
+    LinkedStack s4;
+    while(!s3.isEmpty()){
+        s4.push(s3.pop());
+    }
+    return s4;
+}
 
 
 int main() {
@@ -121,13 +148,27 @@ int main() {
 
     // ── Test Part B ──────────────────────────────────
     cout << "=== Linked Stack ===" << endl;
-    LinkedStack s2;
-    s2.push(10);
-    s2.push(20);
-    s2.push(30);
+    LinkedStack s3;
+    // s2.push(10);
+    // s2.push(20);
+    // s2.push(30);
     // cout << "Top: " << s2.peek() << endl;        // 30
-    cout << "Popped: " << s2.pop() << endl;      // 30
-    cout << "Size: " << s2.size() << endl;        // 2
+    // cout << "Popped: " << s2.pop() << endl;      // 30
+    // cout << "Size: " << s2.size() << endl;        // 2
 
-    return 0;
+//     s3.push(1); s3.push(2); s3.push(3);
+//     LinkedStack r = reverse(s3);
+//     cout << r.pop() << endl;  // 1
+//     cout << r.pop() << endl;  // 2
+//     cout << r.pop() << endl;  // 3
+//     return 0;
+// }
+
+
+LinkedStack a, b;
+a.push(5); a.push(15); a.push(25);
+b.push(65); b.push(75); b.push(85);
+LinkedStack m = merge(a, b);
+while (!m.isEmpty()) cout << m.pop() << " ";
+cout << endl;
 }
